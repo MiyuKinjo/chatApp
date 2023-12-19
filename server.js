a073d8c55e1cc962383c140616d40f77354840af
e215751 <e215751@ie.u-ryukyu.ac.jp>
2023-12-19 14:13:21 +0900

    ユーザー名ありで投稿できる

--- server.js
+++ server.js
@@ -5,16 +5,59 @@ const server = http.createServer(app);
 const io = require("socket.io")(server);
 const PORT = 3000;
 
+const messages = [];
+
+let messageIdCounter = 1; // メッセージIDのためのカウンター
+
+//Nodeサーバにアクセスがあるとindex.htmlへ遷移
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
+io.on("connection", function(socket){
+    var loginUsers = [];
+
+    //ログイン処理
     console.log("ユーザーが接続しました");
 
-    socket.on("chat message", (msg) => {
-        // console.log("メッセージ:" + msg);
-        io.emit("chat message", msg);
+    socket.on('login', function(userInfo){
+        loginUsers[userInfo.userID] = userInfo.userName;
+    });
+
+    //メッセージ処理
+    socket.on("chat message", function(msg){
+        userName = loginUsers[socket.id];
+        io.emit('chat message', {
+            userName: userName,
+            message: msg
+        });
+    
+
+
+        // // 新しいメッセージのIDを生成
+        // const messageId = messageIdCounter++;
+        
+        // // クライアントに送信するメッセージオブジェクト
+        // const messageObject = {
+        //     id: messageId,
+        //     text: msg.text,
+        //     replies: [], // リプライの格納先
+        // };
+
+        // // 親コメントへの返信の場合
+        // if (msg.replyTo) {
+        //     const parentMessage = messages.find((m) => m.id === msg.replyTo);
+        //     if (parentMessage) {
+        //         parentMessage.replies.push(messageObject);
+        //     }
+        // } else {
+        //     // メインのメッセージとして保存
+        //     messages.push(messageObject);
+        // }
+
+        // io.emit("chat message", messageObject);
+
+        // // io.emit("chat message", msg);
     });
 });
 
